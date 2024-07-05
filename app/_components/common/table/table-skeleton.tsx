223dfd9fdaf97b9e6f202522a4327ec23142917a
Jagdish Chandra <jagdishchandra1368@gmail.com>
2024-07-05 21:30:59 +0530

    fix table skeleton

--- app/_components/common/table/table-skeleton.tsx
+++ app/_components/common/table/table-skeleton.tsx
@@ -6,26 +6,37 @@ import { TableRow } from "./table-row";
 
 const TableSkeleton = <T,>({ columns }: TableSkeletonProps<T>) => {
   return (
-    <TableRow>
-      {new Array().fill(12).map((_, idx) =>
-        columns.map(() => (
-          <TableCell key={idx}>
-            <Skeleton
+    <>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((_, idx) => (
+        <TableRow
+          key={idx}
+          className={cn(
+            "flex items-center justify-center gap-1 px-1 pt-1 border-none"
+          )}
+        >
+          {columns.map((column) => (
+            <TableCell
+              key={column.id + idx + _}
               className={cn(
-                "w-full h-[50px] p-4 rounded-sm flex items-center justify-center bg-neutral-500/15 gap-1"
+                "flex items-center justify-center gap-1",
+                column.className
               )}
             >
               <Skeleton
                 className={cn(
-                  "w-full h-[20px] rounded-sm bg-neutral-500/25",
+                  "w-full h-[50px] p-4 rounded-sm bg-neutral-500/35",
                   idx % 2 === 0 && "animate-pulse"
                 )}
-              />
-            </Skeleton>
-          </TableCell>
-        ))
-      )}
-    </TableRow>
+              >
+                <Skeleton
+                  className={cn("w-full h-[20px] rounded-sm bg-neutral-500/25")}
+                />
+              </Skeleton>
+            </TableCell>
+          ))}
+        </TableRow>
+      ))}
+    </>
   );
 };
 
